e1bf921456bd74878a630a5a4f3fc9d93eefd862
Jeremiah Ashley <jeremiah.ashley@live.com>
2020-01-10 23:12:44 -0600

    Get things up to date with tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module',
   },
   extends: [
     '@nuxtjs',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:vue/essential',
     'prettier',
     'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  plugins: ['prettier', 'vue'],
   rules: {
-  }
+    quotes: ['error', 'backtick'],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 }
